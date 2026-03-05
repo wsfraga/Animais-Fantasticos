a0ac00c71061ef3a1433eee679e594e920b4977d
Wesley Fraga <wesleyrodriguesfraga14@gmail.com>
2026-03-05 12:01:56 -0300

    ajustando o codido do tabmenu

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent section");
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-if(tabMenu.length && tabContent.length) {
-  tabContent[0].classList.add("ativo");
-  
-  function activeTab(index) {
-  tabContent.forEach((section) => {
-    section.classList.remove("ativo")
-  });
-  tabContent[index].classList.add("ativo");
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
+
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo")
+      });
+      tabContent[index].classList.add("ativo");
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
 }
 
-tabMenu.forEach((itemMenu, index) => {
-  itemMenu.addEventListener("click", () => {
-    activeTab(index);
-  });
-});
-}
+initTabNav();
