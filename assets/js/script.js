172f3b9a56492b248c45e3078e57e437f168d2fd
Wesley Fraga <wesleyrodriguesfraga14@gmail.com>
2026-04-10 15:04:00 -0300

    finalizando o tooltip

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,11 @@ import  initAnimacaoScroll from "./modules/animacao-scroll.js";
 import  initAccordion from "./modules/accordion.js";
 import  initTabNav from "./modules/tab-nav.js";
 import  initModal from "./modules/modal.js";
-import  initToolTip from "./modules/tooltip.js";
+import  initTooltip from "./modules/tooltip.js";
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
 initModal();
-initToolTip();
+initTooltip();
