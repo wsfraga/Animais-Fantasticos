ce9408b1b8388d6cc8801a5f4846bc400fa36b0d
Wesley Fraga <wesleyrodriguesfraga14@gmail.com>
2026-05-04 13:11:17 -0300

    criando a funcao outsideclick e a dropdown-menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ import  initAccordion from "./modules/accordion.js";
 import  initTabNav from "./modules/tab-nav.js";
 import  initModal from "./modules/modal.js";
 import  initTooltip from "./modules/tooltip.js";
+import  initdropdownMenu from "./modules/dropdown-menu.js";
 
 initScrollSuave();
 initAnimacaoScroll();
