9a9a347b67dcaf969f9b500b06fc6a8a17252a35
Wesley Fraga <wesleyrodriguesfraga14@gmail.com>
2026-05-21 13:13:53 -0300

    criando a funcao de menu-mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ import  initTabNav from "./modules/tab-nav.js";
 import  initModal from "./modules/modal.js";
 import  initTooltip from "./modules/tooltip.js";
 import  initdropdownMenu from "./modules/dropdown-menu.js";
+import  initMenuMobile from "./modules/menu-mobile.js";
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -12,3 +13,4 @@ initAccordion();
 initTabNav();
 initModal();
 initTooltip();
+initMenuMobile();
