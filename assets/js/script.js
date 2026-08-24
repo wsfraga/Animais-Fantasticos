6bc7ad0a4996702d6627c019b280a809cc672909
Wesley Fraga <wesleyrodriguesfraga14@gmail.com>
2026-08-24 18:12:49 -0300

    novos arquivos js criados

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,9 @@ import  initModal from "./modules/modal.js";
 import  initTooltip from "./modules/tooltip.js";
 import  initdropdownMenu from "./modules/dropdown-menu.js";
 import  initMenuMobile from "./modules/menu-mobile.js";
-import  initAnimaNumeros from "./modules/anima-numeros.js";
 import  initFuncionamento from "./modules/funcionamento.js";
+import  initFetchAnimais from "./modules/fetch-animais.js";
+import  initFetchbitcoin from "./modules/fetch-bitcoin.js";
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -16,5 +17,6 @@ initTabNav();
 initModal();
 initTooltip();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
+initFetchAnimais();
+initFetchbitcoin();
